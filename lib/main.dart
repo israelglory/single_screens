f31973e053cf2fedbffe3376ec64f4b73d04b190
israelglory <iglory6@gmail.com>
2023-01-08 12:33:20 +0100

    testing background running

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,35 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_background/flutter_background.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:single_screens/bookmarks/bookmark_page.dart';
+import 'package:single_screens/background_location.dart';
 import 'package:single_screens/models/movie_model.dart';
-import 'package:single_screens/movie_listing.dart';
-import 'package:single_screens/welcome.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(MovieAdapter());
   await Hive.openBox<Movie>('Movies');
+  const androidConfig = FlutterBackgroundAndroidConfig(
+    notificationTitle: "flutter_background example app",
+    notificationText:
+        "Background notification for keeping the example app running in the background",
+    notificationImportance: AndroidNotificationImportance.Default,
+    notificationIcon: AndroidResource(
+        name: 'background_icon',
+        defType: 'drawable'), // Default is ic_launcher from folder mipmap
+  );
+  bool success =
+      await FlutterBackground.initialize(androidConfig: androidConfig);
 
   runApp(
-    DevicePreview(
+      /*DevicePreview(
       enabled: !kReleaseMode,
       builder: (context) => const MyApp(), // Wrap your app
-    ),
-    //const MyApp()
-  );
+    ),*/
+      MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/*class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
@@ -35,5 +42,4 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: const BookmarkPage(),
     );
-  }
-}
+  }}*/
